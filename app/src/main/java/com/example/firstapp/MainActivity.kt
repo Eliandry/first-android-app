76ee93174c16850b60770e884f1cedbaa36fded8
eleandry <andreymacn@gmail.com>
2020-11-17 20:45:03 +0300

    Второй экран

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firstapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -19,5 +20,12 @@ class MainActivity : AppCompatActivity() {
         val countString=textView.text.toString()
         val count:Int =1+countString.toInt()
         textView.text=count.toString()
+    }
+    fun randomMe(view: View){
+        val randomIntent=Intent(this, activity_second::class.java)
+        val countString = textView.text.toString()
+        val count:Int =countString.toInt()
+        randomIntent.putExtra(activity_second.TOTAL_COUNT,count)
+        startActivity(randomIntent)
     }
 }
--- app/src/main/java/com/example/firstapp/activity_second.kt
+++ app/src/main/java/com/example/firstapp/activity_second.kt
@@ -2,10 +2,26 @@ package com.example.firstapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+import java.util.*
 
 class activity_second : AppCompatActivity() {
+    companion object{
+        const val TOTAL_COUNT = "total_count"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        showRandomNum()
+    }
+    fun showRandomNum(){
+        val count=intent.getIntExtra(TOTAL_COUNT,0)
+        val random= Random()
+        var randomInt=0
+        if(count>0){
+            randomInt=random.nextInt(count+1)
+        }
+        textViewRandom.text=Integer.toString(randomInt)
+        textViewLabel.text=getString(R.string.random_heading,count)
     }
 }
